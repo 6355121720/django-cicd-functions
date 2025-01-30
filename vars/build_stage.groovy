20dc6faa54ef7462c7f70b2b76d973460d171480
Viral Dobariya <136214270+6355121720@users.noreply.github.com>
2025-01-30 15:05:20 +0530

    Update build_stage.groovy

--- vars/build_stage.groovy
+++ vars/build_stage.groovy
@@ -2,6 +2,6 @@ def call(String imageName, String imageTag){
   sh '''
   echo "building stage"
   sudo docker rm -f $(docker ps -aq) || sudo docker rmi -f $(docker images -aq) || true
-  docker build -t $(imageName):$(imageTag) .
+  docker build -t ${imageName}:${imageTag} .
   '''
 }
